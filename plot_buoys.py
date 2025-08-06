b235754fb1dc77866cc2ca68a15cae509bf0e2ac
Andy Matthews <antt@noc.ac.uk>
2025-08-06 11:38:03 +0000

    Added license to code

--- plot_buoys.py
+++ plot_buoys.py
@@ -1,3 +1,15 @@
+"""
+Copyright 2025 National Oceanography Centre
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+ http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
 import geopandas as gpd
 from matplotlib.offsetbox import AnchoredText
 import matplotlib.pyplot as plt
@@ -25,7 +37,7 @@ def plot_buoy_data():
     ax.set_xlim([bounds[0] - 0.5, bounds[2] + 0.5])
     north_atlantic.plot(ax=ax)
 
-    buoys_geo.plot(ax=ax, color="yellow")
+    buoys_geo.plot(ax=ax, color="red")
 
     axis_labels = []
 
